b4f8a462cec6f33dc705ab99c87e226ea3155fe7
lllbbb <51808638+lijiabad@users.noreply.github.com>
2023-07-07 16:37:08 +0800

    部分提取优化/debug

--- app/controller/nlp.js
+++ app/controller/nlp.js
@@ -6,144 +6,185 @@ const Data = require("./rg");
 var AipNlpClient = require("baidu-aip-sdk").nlp;
 class NlpController extends Controller {
     async nameParse(result, input_text ,new_json) {
-        const text = input_text;
-        const data = JSON.parse(new_json);
-        const wordsResult = data.result.words_result;
+        console.log('this this this ');
+        console.log(typeof new_json);
         const baijiaxing = ['赵','荆','刘','钱','洪','林','赖','孙','吉','叶','李', '周', '吴', '郑', '王', '冯', '陈', '褚', '卫', '蒋', '沈', '韩', '杨', '朱', '秦', '尤', '许', '何', '吕', '施', '张', '孔', '曹', '严', '华', '金', '魏', '陶', '姜', '戚', '谢', '邹', '喻', '柏', '水', '窦', '章', '云', '苏', '潘', '葛', '奚', '范', '彭', '郎', '鲁', '韦', '昌', '马', '苗', '凤', '花', '方', '俞', '任', '袁', '柳', '酆', '鲍', '史', '唐', '费', '廉', '岑', '薛', '雷', '贺', '倪', '汤', '滕', '殷', '罗', '毕', '郝', '邬', '安', '常', '乐', '于', '时', '傅', '皮', '卞', '齐', '康', '伍', '余', '元', '卜', '顾', '孟', '平', '黄', '和', '穆', '萧', '尹', '姚', '邵', '湛', '汪', '祁', '毛', '禹', '狄', '米', '贝', '明', '臧', '计', '伏', '成', '戴', '谈', '宋', '茅', '庞', '熊', '纪', '舒', '屈', '项', '祝', '董', '梁'];
         let foundName = false;  // 姓或名是否已找到的标志
         const NAME = [];
+        console.log(new_json);
+        console.log(typeof new_json);
 
-         for (let item of wordsResult){
-             const words = item.words;
-             if (words && (words.includes('姓') || words.includes('名') || words.includes('姓名'))){
-                 const index = words.indexOf('：');
-                 if (index !== -1){
-                     const result = words.slice(index + 1).trim();
-                     if (!NAME.includes(result)){
-                         NAME.push(result);
-                     }
-                 }
-                 foundName = true;
-                 break;
-             }
-         }
-         // if (!foundName){
-         //     for (let item of wordsResult){
-         //         const words = item.words;
-         //         if (words && (words.length === 2 || words.length === 3)){
-         //             const cleanedWords = words.replace(/\s/g, ''); // 去除空格
-         //             if (/^[\u4e00-\u9fa5]{2,3}$/.test(cleanedWords)){
-         //                 const firstChar = cleanedWords.charAt(0);
-         //                 if (baijiaxing.includes(firstChar)){
-         //                     NAME.push(cleanedWords);
-         //                 }
-         //             }
-         //         }
-         //     }
-         // }
-         const parts = text.split(' ');
-         const filteredParts = parts.filter(part => part.length === 2 || part.length === 3 );
-         console.log(filteredParts);
-         for (let i = 0; i < filteredParts.length; i++){
-             const firstChar = filteredParts[i][0];
-             if (baijiaxing.includes(firstChar)){
-                 NAME.push(filteredParts[i]);
-             }
-         }
-         console.log('----------------');
-         console.log(NAME);
-         return NAME;
+        if(typeof new_json !== "function"){
+            console.log(new_json);
+            const data = JSON.parse(new_json);
+            const wordsResult = data.result.words_result;
+            for (let item of wordsResult){
+                const words = item.words;
+                if (words && (words.includes('姓') || words.includes('名') || words.includes('姓名'))){
+                    const index = words.indexOf('：');
+                    if (index !== -1){
+                        const result = words.slice(index + 1).trim();
+                        if (!NAME.includes(result)){
+                            NAME.push(result);
+                        }
+                    }
+                    foundName = true;
+                    break;
+                }
+            }
+            for (let i of result.items) {
+                if (i['ne'] === 'PER' || i['pos'] === 'nr') {
+                    NAME.push(i['item']);
+                }
+            }
+        } else {
+            for (let i of result.items) {
+                if (i['ne'] === 'PER' || i['pos'] === 'nr') {
+                    NAME.push(i['item']);
+                }
+            }
+            const items = result.items;
+            for (const item of items) {
+                const words = item.item;
+                if (words && (words.includes('姓') || words.includes('名') || words.includes('姓名'))){
+                    const index = words.indexOf('：');
+                    if (index !== -1){
+                        const result = words.slice(index + 1).trim();
+                        if (!NAME.includes(result)){
+                            NAME.push(result);
+                        }
+                    }
+                    foundName = true;
+                    break;
+                }
+            }
+        }
+        const parts = input_text.split(' ');
+        const filteredParts = parts.filter(part => part.length === 2 || part.length === 3 );
+        console.log(filteredParts);
+        for (let i = 0; i < filteredParts.length; i++){
+            const firstChar = filteredParts[i][0];
+            if (baijiaxing.includes(firstChar)){
+                NAME.push(filteredParts[i]);
+            }
+        }
+        console.log('----------------');
+        console.log(NAME);
+        return NAME;
     }
 
+    async getTimeStamp(result, input_text) {
+        //先提取时间戳 NLP或正则提取
+        let time_result = []
+        //nlp标签提取 很有可能提取不到
+        for (let i of result.items) {
+            if (i['ne'] === 'TIME' || i['pos'] === 't' || i['pos'] === 'w') {
+                time_result.push(i['item']);
+            }
+        }
+
+        //正则表达式提取
+        const pattern = /\b\d{4}\b/g; // 匹配四位数字 超过四位不匹配
+        const matches = input_text.match(pattern);
+        var got_years = matches.filter(match => parseInt(match) >= 1950 && parseInt(match) <= 9999);
+        console.log("got_years的类型", typeof got_years);
+        return got_years;
+    }
 
     async ageParse(result, input_text, new_json) {
         let AGE = [];
-        const data = JSON.parse(new_json);
-        const wordsResult = data.result.words_result;
         const regex1 = /(?:出生年月|生日|出生|生)[:：]\s*([\d./-]+)/;
         // const regex2 = /[^岁]*(\d{2})[^岁]*/g;
         let regexAge = /年龄：(\d{2})/;
         let regexYears = /(\d{2})岁/;
         const regex3 = /\b(\d{2})\b/g;
         const regex4 = /\b(\d{4}(?:[-./]\d{2}){2})\b/;
-        for (let i = 0; i < wordsResult.length; i++){
-            const words = wordsResult[i].words;
-            const match1 = words.match(regex1);
-            if (match1 && match1[1]){
-                const birthDate = match1[1];
-                const year = parseInt(birthDate.split(/[./-]/)[0]);
-                const currentYear = new Date().getFullYear();
-                const age = currentYear - year + 1;
-                AGE.push(age);
-                break;
-            }
-        }
-        // if (AGE.length === 0){
-        //     for (let i = 0; i < wordsResult.length; i++){
-        //         const words = wordsResult[i].words;
-        //         const matches2 = words.match(regex2);
-        //         if (matches2){
-        //             for (let j = 0; j < matches2.length; j++){
-        //                 const ageMatch = matches2[j].match(/\d{2}/);
-        //                 if (ageMatch){
-        //                     const age = parseInt(ageMatch[0]);
-        //                     AGE.push(age);
-        //                 }
-        //             }
-        //         }
-        //         if (AGE.length>0){
-        //             break;
-        //         }
-        //     }
-        // }
-        if (AGE.length === 0){
+        if(typeof new_json !== "function"){
+            const data = JSON.parse(new_json);
+            const wordsResult = data.result.words_result;
             for (let i = 0; i < wordsResult.length; i++){
                 const words = wordsResult[i].words;
-                const matchAge = words.match(regexAge);
-                if (matchAge && matchAge[1]){
-                    const age = parseInt(matchAge[1]);
-                    AGE.push(age);
-                    break;
-                }
-                const matchYears = words.match(regexYears);
-                if (matchYears && matchYears[1]){
-                    const age = parseInt(matchYears[1]);
-                    AGE.push(age);
-                    break;
-                }
-            }
-        }
-
-        if (AGE.length === 0){
-            for (let i = 0; i < wordsResult.length; i++){
-                const words = wordsResult[i].words;
-                const matches3 = words.match(regex3);
-                if (matches3){
-                    for (let j = 0; j < matches3.length; j++){
-                        const ageMatch = matches3[j].match(/\d{2}/);
-                        if (ageMatch){
-                            const age = parseInt(ageMatch[0]);
-                            AGE.push(age);
-                        }
-                    }
-                }
-                if (AGE.length === 0){
-                    break;
-                }
-            }
-        }
-
-        if (AGE.length === 0){
-            for (let i = 0; i < wordsResult.length; i++){
-                const words = wordsResult[i].words;
-                const match4 = words.match(regex4);
-                if (match4 && match4[1]){
-                    const birthDate = match4[1];
+                const match1 = words.match(regex1);
+                if (match1 && match1[1]){
+                    const birthDate = match1[1];
                     const year = parseInt(birthDate.split(/[./-]/)[0]);
                     const currentYear = new Date().getFullYear();
                     const age = currentYear - year + 1;
                     AGE.push(age);
                     break;
+                }
+            }
+            if (AGE.length === 0){
+                for (let i = 0; i < wordsResult.length; i++){
+                    const words = wordsResult[i].words;
+                    const matchAge = words.match(regexAge);
+                    if (matchAge && matchAge[1]){
+                        const age = parseInt(matchAge[1]);
+                        AGE.push(age);
+                        break;
+                    }
+                    const matchYears = words.match(regexYears);
+                    if (matchYears && matchYears[1]){
+                        const age = parseInt(matchYears[1]);
+                        AGE.push(age);
+                        break;
+                    }
+                }
+            }
+    
+            if (AGE.length === 0){
+                for (let i = 0; i < wordsResult.length; i++){
+                    const words = wordsResult[i].words;
+                    const matches3 = words.match(regex3);
+                    if (matches3){
+                        for (let j = 0; j < matches3.length; j++){
+                            const ageMatch = matches3[j].match(/\d{2}/);
+                            if (ageMatch){
+                                const age = parseInt(ageMatch[0]);
+                                AGE.push(age);
+                            }
+                        }
+                    }
+                    if (AGE.length === 0){
+                        break;
+                    }
+                }
+            }
+    
+            if (AGE.length === 0){
+                for (let i = 0; i < wordsResult.length; i++){
+                    const words = wordsResult[i].words;
+                    const match4 = words.match(regex4);
+                    if (match4 && match4[1]){
+                        const birthDate = match4[1];
+                        const year = parseInt(birthDate.split(/[./-]/)[0]);
+                        const currentYear = new Date().getFullYear();
+                        const age = currentYear - year + 1;
+                        AGE.push(age);
+                        break;
+                    }
+                }
+            }
+        } else {
+            var got_years = [];
+            const items = result.items;
+            for (const item of items) {
+                const words = item.item;
+                const match1 = words.match(regex1);
+                if (match1 && match1[1]){
+                    const birthDate = match1[1];
+                    const year = parseInt(birthDate.split(/[./-]/)[0]);
+                    const currentYear = new Date().getFullYear();
+                    const age = currentYear - year + 1;
+                    AGE.push(age);
+                    break;
+                }
+            }
+            got_years = await this.getTimeStamp(result, input_text);
+            for (let i = 0; i < got_years.length; i++) {
+                var age = 2023 - got_years[i];
+                if (age > 0) {
+                    AGE.push(age);
                 }
             }
         }
@@ -174,8 +215,6 @@ class NlpController extends Controller {
         GRADUATE.push(lastMatchedItem);
         return GRADUATE;
     }
-
-
 
     async eduParse(result, input_text) { //查找院校字典对应的属性  小学0、初中1、中专2、高中3、专科4、本科5、(硕士)研究生6、(博士研究生)博士7 
         var EDUCTION = [];
